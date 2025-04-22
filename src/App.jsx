e967702f8d2f4328fc6a46faa8d0cfe5984fdbd8
AkmMohanapriyan <mohanapriyanpriyan4@gmail.com>
2025-04-22 16:32:25 +0530

    first React Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './assets/component/Header'
+import Section from './assets/component/Section'  
+import Footer from './assets/component/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+<Header/>
+<Section/>
+<Footer/>
+      
+    </>
+  )
+}
+
+export default App
